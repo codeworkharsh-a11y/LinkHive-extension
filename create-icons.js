8cb3b2d0fd2de7d8a3c32d3699938244e22778a3
Harsh Malaviya <codework.harsh@gmail.com>
2026-08-04 13:09:15 +0530

    chore: Clean up repository to include only core extension files and documentation

--- create-icons.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const pngBase64 = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAANSURBVBhXY3jP4PgfAAWpA50G65xAAAAAAElFTkSuQmCC";
-const buf = Buffer.from(pngBase64, 'base64');
-fs.writeFileSync('icons/icon16.png', buf);
-fs.writeFileSync('icons/icon48.png', buf);
-fs.writeFileSync('icons/icon128.png', buf);
-console.log('Placeholder icons created.');
